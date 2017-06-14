769f29d1db5a49ae5f530131ccedf8dd555b9d9b
SURFACEDJUR <thekookaburraz@gmail.com>
2017-06-14 04:52:37 +0200

    yml

--- target/generated-sources/annotations/exercise/realdatabase/travis/SystemUser_.java
+++ target/generated-sources/annotations/exercise/realdatabase/travis/SystemUser_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-14T04:19:15")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-14T04:47:30")
 @StaticMetamodel(SystemUser.class)
 public class SystemUser_ { 
 
